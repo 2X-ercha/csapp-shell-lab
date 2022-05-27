0c78ee729a125e741c955da55c07f5ef25696998
noionion <noionion@outlook.com>
2022-05-27 11:39:59 +0800

    waitfg sigchld_handler, test5 OK, test4 job def fix

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -165,18 +165,26 @@ int main(int argc, char **argv)
 */
 /*
 trace01.txt – 在 EOF 处正确地停止。
-eval 函数是根据输入命令行进行对应操作的函数。在 CSAPP 2e 8.4.6 节（中文版 P502 / 英文版 P733），有 eval 函数的大致框架。
-trace01 只需要正确响应 EOF 就可以了。在课本的程序框架中，已经使用 parseline 函数将命令行解析为了参数，并判断了第一个参数是否为 NULL。
+  eval 函数是根据输入命令行进行对应操作的函数。在 CSAPP 2e 8.4.6 节（中文版 P502 / 英文版 P733），有 eval 函数的大致框架。
+  trace01 只需要正确响应 EOF 就可以了。在课本的程序框架中，已经使用 parseline 函数将命令行解析为了参数，并判断了第一个参数是否为 NULL。
 
-显然，第一个参数是 NULL，后面当然更是 NULL，意味着没有输入。也就是说，我们什么额外的工作都不用干……
+  显然，第一个参数是 NULL，后面当然更是 NULL，意味着没有输入。也就是说，我们什么额外的工作都不用干……
 
 trace03.txt – 运行一个前台任务。
 trace04.txt – 运行一个后台任务。
-在课本的 eval 框架中，首先使用 builtin_cmd 判断并处理内置函数，然后根据 bg 值（由 parseline 得到）判断是否在后台运行。
-这其中会先 fork 出一个子进程，然后使用 execve 执行目标程序。
-请注意，只有子进程会在运行失败时 exit。根据是否是前台任务，判断是直接打印执行详情还是等待前台进程结束。
+  在课本的 eval 框架中，首先使用 builtin_cmd 判断并处理内置函数，然后根据 bg 值（由 parseline 得到）判断是否在后台运行。
+  这其中会先 fork 出一个子进程，然后使用 execve 执行目标程序。
+  请注意，只有子进程会在运行失败时 exit。根据是否是前台任务，判断是直接打印执行详情还是等待前台进程结束。
 
-所以还是啥都不用干。。。(job不一致，trace05解决)
+  所以还是啥都不用干。。。(job不一致，trace05解决)
+
+trace05.txt – 处理 jobs 内置命令。
+运行 jobs 都不会输出任何东西。毕竟在我们已有的代码里，既没有在任务开始时将其添加到任务列表，也没有在结束时将它移出。
+要做到将任务添加到任务列表，需要在 fork 后调用 addjob
+如果当前任务需要在前台运行，需要等待前台任务（如果有）结束，但 tsh 有一个 waitfg 函数，看起来他们不想让我们把等待工作放到 eval 中。
+另外还要处理 eval 中的信号问题。Writeup 中提到
+  > 在 eval 中，父进程在 fork 子进程之前，必须使用 sigprocmask 函数来阻断 SIGCHLD 信号，然后在使用 addjob 将子进程加入任务列表之后，再调用 sigprocmask 恢复 SIGCHLD 信号。因为子进程继承了父进程的中断向量，所以子进程必须在它执行新程序之前将 SIGCHILD 恢复。
+  > 父进程这样将 SIGCHLD 信号阻断，是为了避免子进程被 SIGCHLD 处理程序回收（然后被从任务列表中移除），之后父进程调用 addjob 时的竞态条件。
 */
 void eval(char *cmdline)
 {
@@ -184,6 +192,11 @@ void eval(char *cmdline)
     char buf[MAXLINE]; // command will be parsed and modified?
     int bg;            // whether it runs in background
     pid_t pid;
+
+    // trace05 add
+    sigset_t mask;
+    sigemptyset(&mask);
+
     // preprocess cmd line
     strcpy(buf, cmdline);
     bg = parseline(buf, argv); // convert the command into argv
@@ -194,22 +207,38 @@ void eval(char *cmdline)
     // run external command
     if (!builtin_cmd(argv))
     {
+        // trace05 add
+        sigaddset(&mask, SIGCHLD);
+        sigprocmask(SIG_BLOCK, &mask, NULL); // 阻断 SIGCHLD 信号
+
         if ((pid = fork()) == 0) // this is child
         {
+            // trace05 add
+            sigprocmask(SIG_UNBLOCK, &mask, NULL); // 恢复信号
+
             if (execve(argv[0], argv, environ) < 0) // execute command failed
             {
                 printf("%s: Command not found\n", argv[0]);
                 exit(0); // here only child exited
             }
         }
+        addjob(jobs, pid, bg ? BG : FG, cmdline); // add the job to job list.
+
+        // trace05 add
+        sigprocmask(SIG_UNBLOCK, &mask, NULL); // 父进程 addjob 完毕后也要恢复
+
+        // 代码的 addjob 中第三个参数 state 有三个取值，FG=1、BG=2、ST=3。虽然直接使用 bg+1 也是可行的方案，但这样使用三元运算符会更优雅更容易理解。
         if (!bg) // run the process in foreground:
         // wait for foreground job to terminate
         {
+            waitfg(pid); // the waiting stuff should be done in `waitfg`
+            /* trace05 修改
             int status;
             if (waitpid(pid, &status, 0) < 0) // if return -1, then waiting failed
             {
                 unix_error("waitfg: waitpid error");
             }
+            */
         }
         else
         {
@@ -284,11 +313,22 @@ int parseline(const char *cmdline, char **argv)
 /*
 trace02.txt – 处理内置的 quit 命令。
 在 tsh 中，内置的命令实在 builtin_cmd 函数中处理的。只需要在其中判断一下第一个参数是否为 quit，如果是的话退出即可。
+
+trace05.txt – 处理 jobs 内置命令。
+将处理 jobs 命令的地方做好。tsh 已经为我们实现了 listjobs 函数，可以直接放到 builtin_cmd 中。
+注意 return 1 用来告诉 eval 已经找到了一个内置命令，否则会提示 “command not found”。
 */
 int builtin_cmd(char **argv)
 {
     if (strcmp(argv[0], "quit") == 0) // process quit command
       exit(0);
+
+    if (strcmp(argv[0], "jobs") == 0) // t5: process jobs command
+    {
+        listjobs(jobs);
+        return 1; // this IS a builtin command, return 1 to notify
+    }
+
     return 0;
 }
 
@@ -303,8 +343,19 @@ void do_bgfg(char **argv)
 /*
  * waitfg - Block until process pid is no longer the foreground process
  */
+/*
+trace05.txt – 处理 jobs 内置命令。
+Writeup 提示
+  > 实验有一个棘手的部分，是决定 waitfg 和 sigchld 处理函数之间的工作分配。我们推荐以下方法：
+  > – 在 waitfg 中，用一个死循环包裹 sleep 函数。
+  > – 在 sigchild_handler 中，调用且仅调用一次 waitpid。
+*/
 void waitfg(pid_t pid)
 {
+    while (pid == fgpid(jobs))
+    {
+        sleep(0);
+    }
     return;
 }
 
@@ -319,8 +370,25 @@ void waitfg(pid_t pid)
  *     available zombie children, but doesn't wait for any other
  *     currently running children to terminate.
  */
+/*
+trace05.txt – 处理 jobs 内置命令。
+涉及到对 waitpid 函数的更深入理解，在 CSAPP 8.4.3 节（中文版 P496，英文版 P724），提到了这样一段话，介绍了 waitpid 函数的默认行为，以及退出状态的检查方法
+  > （更改默认行为）WNOHANG|WUNTRACED：立即返回。如果等待集里面没有子进程已经终止，那么返回 0；否则，返回其中一个已终止子进程的 PID。
+  > （检查回收子进程的返回状态）WIFEXITED(status)：如果子进程正常退出，即通过调用 exit 或者 return，则返回真。
+Writeup 中也提到 WNOHANG|WUNTRACED 或许会有用。
+前一个更改默认行为的部分对应了 waitpid 的第三个参数，后一个检查用于确定是否有个子进程真的退出了（而非没有子进程终止，waitpid 返回了 0）
+*/
 void sigchld_handler(int sig)
 {
+    pid_t pid;
+    int status;
+    while((pid=waitpid(-1,&status,WNOHANG|WUNTRACED))>0) // check if a child has become zombie, without wait
+    {
+        if(WIFEXITED(status))
+        {
+            deletejob(jobs,pid); // remove pid from job list
+        }
+    }
     return;
 }
 
